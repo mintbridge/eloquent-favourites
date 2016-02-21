e8c3c3a7b3f6d16f145a8cefa007f5247d468b99
Paul Dixon <paul.dixon@bedegaming.com>
2016-02-21 00:08:34 +0000

    Use methods to get favouritable data
    - add the methods to the interface definition
    - use auth methods for user id
    - remove un-used use statements

--- src/Favouritable.php
+++ src/Favouritable.php
@@ -2,16 +2,31 @@
 
 namespace Mintbridge\EloquentFavourites;
 
-use Auditor;
 use Config;
 
 trait Favouritable
 {
     /**
-     * Get all of the activities performed on the model
+     * Specify the favourites relationship between the models
      */
     public function favourites()
     {
         return $this->morphMany(Config::get('eloquent-favourites.model'), 'favouritable');
+    }
+
+    /**
+     * Get the favouritable id for the model
+     */
+    public function getFavouriteableId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the favouritable type for the model
+     */
+    public function getFavouriteableType()
+    {
+        return get_class($this);
     }
 }
